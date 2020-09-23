c365f1dfcce5b7b520b71a58f8f3e90aae81db8a
Bia camille <biacamille2019@gmail.com>
2020-09-23 17:35:21 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function createHeart() {
+    const heart = document.createElement('div');
+
+    heart.classList.add("heart");
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+    heart.innerHTML = "❤";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() =>{
+        heart.remove();
+    },5000);
+
+    document.write
+
+}
+
+setInterval(createHeart, 300);
